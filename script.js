92f660a1c37c550e39a09307272ad69ea6da82e6
Indy983 <indyindy1802@gmail.com>
2025-01-16 22:50:59 +0700

    Create script.js
    Signed-off-by: Indy983 <indyindy1802@gmail.com>

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var heading = document.querySelector("h1");
+    heading.style.fontSize = "24px";
+    heading.textContent = "Welcome to my website!";
+});
